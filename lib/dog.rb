aedfc58434dcdff787d03a5745c34a57ce99300f
Arianna Kosten <arianna.kosten@gmail.com>
2019-04-28 20:28:15 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,18 @@
 class Dog 
-def initiliaze(breed)
-  @breed = breed
+def initiliaze(name, breed)
+  @breed = "Mutt"
+  @name = name
 end
   def breed=(breed)
     @breed = "Mutt"
   end
   def breed
     @breed
+  end
+  def name=(name)
+    @name = name
+  end
+  def name
+    @name
   end
 end
